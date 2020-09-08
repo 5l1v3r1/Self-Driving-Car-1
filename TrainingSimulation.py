03b10fdcb20f537a9860974b7026cd85ebd5a8eb
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-09-08 23:52:01 +0300

    4.0

--- TrainingSimulation.py
+++ TrainingSimulation.py
@@ -3,11 +3,16 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-
+from sklearn.model_selection import train_test_split
 
 path = "Data"
 data = importDataInfo(path)
 balanceData(data)
 
 imagesPath, steering = loadData(path, data)
-print(imagesPath[0], steering[0])
+# print(imagesPath[0], steering[0])
+
+xTrain, xVal, yTrain, yVal = train_test_split(imagesPath, steering, test_size=0.2, random_state=5)
+print("Total Training Images: ", len(xTrain))
+print("Total Validation Images: ", len(xVal))
+
