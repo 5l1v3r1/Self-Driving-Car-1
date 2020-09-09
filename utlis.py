345233a8e165fa7a10f95841193b67a4827ca806
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-09-09 18:07:24 +0300

    12.0

--- utlis.py
+++ utlis.py
@@ -5,9 +5,12 @@ import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 import matplotlib.image as mpimg
 from imgaug import augmenters as iaa
+import cv2
+
 
 def getName(filePath):
     return filePath.split("\\")[-1]
+
 
 def importDataInfo(path):
     columns = ["Center", "Left", "Right", "Steering", "Throttle", "Brake", "Speed"]
@@ -16,6 +19,7 @@ def importDataInfo(path):
     # print(data["Center"])
     print("Total images imported: ", data.shape[0])
     return data
+
 
 def balanceData(data, display=True):
     nBins = 31
@@ -26,27 +30,28 @@ def balanceData(data, display=True):
         center = (bins[:-1] + bins[1:]) * 0.5
         plt.bar(center, hist, width=0.06)
         plt.plot((-1, 1), (samplesPerBin, samplesPerBin))
-        #plt.show()
+        # plt.show()
 
     removeIndexList = []
     for j in range(nBins):
         binDataList = []
 
         for i in range(len(data["Steering"])):
-            if data["Steering"][i] >= bins[j] and data["Steering"][i] <= bins[j+1]:
+            if data["Steering"][i] >= bins[j] and data["Steering"][i] <= bins[j + 1]:
                 binDataList.append(i)
         binDataList = shuffle(binDataList)
         binDataList = binDataList[samplesPerBin:]
         removeIndexList.extend(binDataList)
     print("Removed images:", len(removeIndexList))
-    data.drop(data.index[removeIndexList], inplace = True)
+    data.drop(data.index[removeIndexList], inplace=True)
     print("Remaining Images:", len(data))
 
     if display:
         hist, _ = np.histogram(data["Steering"], nBins)
         plt.bar(center, hist, width=0.06)
         plt.plot((-1, 1), (samplesPerBin, samplesPerBin))
-        #plt.show()
+        # plt.show()
+
 
 def loadData(path, data):
     imagesPath = []
@@ -54,21 +59,34 @@ def loadData(path, data):
 
     for i in range(len(data)):
         indexedData = data.iloc[i]
-        #print(indexedData)
+        # print(indexedData)
         imagesPath.append(os.path.join(path, "IMG", indexedData[0]))
-        #print(os.path.join(path, "IMG", indexedData[0]))
+        # print(os.path.join(path, "IMG", indexedData[0]))
         steering.append(float(indexedData[3]))
 
     imagesPath = np.asarray(imagesPath)
     steering = np.asarray(steering)
     return imagesPath, steering
 
+
 def augmentImage(imgPath, steering):
     img = mpimg.imread(imgPath)
-    pan = iaa.Affine(translate_percent={"x":(-0.1, 0.1), "y":(-0.1, 0.1)})
-    img = pan.augment_image(img)
+
+    if np.random.rand() < 0.5:
+        pan = iaa.Affine(translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)})
+        img = pan.augment_image(img)
+    if np.random.rand() < 0.5:
+        zoom = iaa.Affine(scale=(1, 1.2))
+        img = zoom.augment_image(img)
+    if np.random.rand() < 0.5:
+        brightness = iaa.Multiply((0.5, 1.2))
+        img = brightness.augment_image(img)
+    if np.random.rand() < 0.5:
+        img = cv2.flip(img, 1)
+        steering = -steering
+    return img, steering
 
 
-
-
-
+# imgRe, st = augmentImage("test.jpg", 0)
+# plt.imshow(imgRe)
+# plt.show()
