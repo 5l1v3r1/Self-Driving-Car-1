043ede3897e115a9267ae3d037961931cc113204
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-09-12 19:40:05 +0300

    16.0

--- utlis.py
+++ utlis.py
@@ -90,3 +90,16 @@ def augmentImage(imgPath, steering):
 # imgRe, st = augmentImage("test.jpg", 0)
 # plt.imshow(imgRe)
 # plt.show()
+
+def preProcessing(img):
+    img = img[60:135, :, :]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img / 255
+    return img
+
+
+imgRe = preProcessing(mpimg.imread("test.jpg"))
+plt.imshow(imgRe)
+plt.show()
